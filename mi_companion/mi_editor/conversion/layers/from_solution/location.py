e9a61ce6a9c17400cefc36799d1f7e5d89785529
Christian Heider Nielsen <mrnaah@gmail.com>
2024-06-07 13:20:51 +0200

    fix floor reference

--- mi_companion/mi_editor/conversion/layers/from_solution/location.py
+++ mi_companion/mi_editor/conversion/layers/from_solution/location.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Any, List, Optional
+from typing import Any, Iterable, List, Optional
 
 import geopandas
 from jord.qlive_utilities import add_dataframe_layer
@@ -34,6 +34,8 @@ from ...projection import (
 
 logger = logging.getLogger(__name__)
 
+USE_EXTERNAL_ID_FLOOR_SELECTION = False
+
 
 def add_location_layer(
     collection_: CollectionMixin,
@@ -46,30 +48,46 @@ def add_location_layer(
     dropdown_widget: Optional[Any] = None,
 ) -> Optional[List[Any]]:  # QgsVectorLayer
     shape_df = to_df(collection_)
+
+    assert len(collection_) == len(shape_df)
     if shape_df.empty:
         logger.warning(f"{name=} {shape_df=} was empty!")
 
         return
 
-    shape_df = shape_df[shape_df["floor.external_id"] == floor.external_id]
+    # logger.info(f"adding {name=} with {len(collection_)} elements")
 
-    locations_df = geopandas.GeoDataFrame(
-        shape_df[
-            [
-                c
-                for c in shape_df.columns
-                if ("." not in c)
-                or ("location_type.name" == c)
-                or (
-                    "custom_properties." in c
-                    and (
-                        ".custom_properties" not in c
-                    )  # Only this objects custom_properties
-                )
-            ]
-        ],
-        geometry=geom_type,
-    )
+    if USE_EXTERNAL_ID_FLOOR_SELECTION:  # OLD WAY
+        floor_selection = shape_df["floor.external_id"] == floor.external_id
+    else:
+        floor_selection = (shape_df["floor.floor_index"] == floor.floor_index) & (
+            shape_df["floor.building.external_id"] == floor.building.external_id
+        )  # TODO: USE Floor.compute_key instead
+
+    shape_df = shape_df[floor_selection]
+
+    if len(shape_df) == 0:
+        # logger.warning(f"No location were found for {floor.__desc__}")
+
+        return
+
+    column_selection = [
+        c
+        for c in shape_df.columns
+        if ("." not in c)
+        or ("location_type.name" == c)
+        or (
+            "custom_properties." in c
+            and (".custom_properties" not in c)  # Only this objects custom_properties
+        )
+    ]
+
+    if column_selection:
+        selected = shape_df[column_selection]
+    else:
+        selected = shape_df
+
+    locations_df = geopandas.GeoDataFrame(selected, geometry=geom_type)
     assert len(shape_df) == len(
         locations_df
     ), f"Some Features where dropped, should not happen! {len(shape_df)}!={len(locations_df)}"
@@ -84,6 +102,10 @@ def add_location_layer(
         locations_df
     ), f"Some Features where dropped, should not happen! {len(shape_df)}!={len(locations_df)}"
 
+    if not len(shape_df):
+        # logger.warning(f"Nothing to be added, skipping {name}")
+        return
+
     added_layers = add_dataframe_layer(
         qgis_instance_handle=qgis_instance_handle,
         dataframe=locations_df,
@@ -94,14 +116,21 @@ def add_location_layer(
         crs=f"EPSG:{GDS_EPSG_NUMBER if should_reproject() else MI_EPSG_NUMBER }",
     )
 
+    layer = None
     for a in added_layers:
         if a:
-            layer = next(iter(a))
-            break
+            if isinstance(a, Iterable):
+                for i in a:
+                    if i:
+                        layer = i
+                        break
+                if layer:
+                    break
+            else:
+                layer = a
+                break
     else:
-        logger.warning(
-            f"Did not add any {geom_type} layers for {name}:{floor.__desc__}!"
-        )
+        # logger.warning(            f"Did not add any {geom_type} layers for {name}:{floor.__desc__}!"        )
         return
 
     assert (
