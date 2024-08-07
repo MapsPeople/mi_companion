7d83f39ba9398e192cc788a9590616cac0084755
Christian Heider Nielsen <mrnaah@gmail.com>
2024-08-07 14:05:56 +0200

    bundle importlib

--- mi_companion/constants.py
+++ mi_companion/constants.py
@@ -1,7 +1,6 @@
 import logging
-from pathlib import Path
-
 import site  # https://docs.python.org/3/library/site.html#module-site
+from pathlib import Path
 
 BUNDLED_PACKAGES_DIR = "mi_companion_bundle"
 
@@ -9,33 +8,29 @@ relative_bundled_packages_dir = Path(__file__).parent.parent / BUNDLED_PACKAGES_
 logger = logging.getLogger(__name__)
 
 if relative_bundled_packages_dir.exists():
-    logger.info(f"Loading {relative_bundled_packages_dir}")
-    site.addsitedir(str(relative_bundled_packages_dir))
-
+  logger.info(f"Loading {relative_bundled_packages_dir}")
+  site.addsitedir(str(relative_bundled_packages_dir))
 
 def read_author_from_metadata(metadata_file: Path) -> str:
-    with open(metadata_file) as f:
-        for l in f.readlines():
-            if "author=" in l:
-                return l.split("=")[-1].strip()
-    raise Exception(f"Did not find version in {metadata_file=}")
-
+  with open(metadata_file) as f:
+    for l in f.readlines():
+      if "author=" in l:
+        return l.split("=")[-1].strip()
+  raise Exception(f"Did not find version in {metadata_file=}")
 
 def read_project_name_from_metadata(metadata_file: Path) -> str:
-    with open(metadata_file) as f:
-        for l in f.readlines():
-            if "name=" in l:
-                return l.split("=")[-1].strip()
-    raise Exception(f"Did not find version in {metadata_file=}")
-
+  with open(metadata_file) as f:
+    for l in f.readlines():
+      if "name=" in l:
+        return l.split("=")[-1].strip()
+  raise Exception(f"Did not find version in {metadata_file=}")
 
 def read_version_from_metadata(metadata_file: Path) -> str:
-    with open(metadata_file) as f:
-        for l in f.readlines():
-            if "version=" in l:
-                return l.split("=")[-1].strip()
-    raise Exception(f"Did not find version in {metadata_file=}")
-
+  with open(metadata_file) as f:
+    for l in f.readlines():
+      if "version=" in l:
+        return l.split("=")[-1].strip()
+  raise Exception(f"Did not find version in {metadata_file=}")
 
 PLUGIN_DIR = Path(__file__).parent
 METADATA_FILE = PLUGIN_DIR / "metadata.txt"
@@ -44,24 +39,23 @@ PROJECT_NAME = read_project_name_from_metadata(METADATA_FILE)
 VERSION = read_version_from_metadata(METADATA_FILE)
 PLUGIN_AUTHOR = read_author_from_metadata(METADATA_FILE)
 
-from jord.qt_utilities import DockWidgetAreaFlag
-
 try:
-    from apppath import AppPath
+  from apppath import AppPath
 
-    PROJECT_APP_PATH = AppPath(PROJECT_NAME, app_author=PLUGIN_AUTHOR)
+  PROJECT_APP_PATH = AppPath(PROJECT_NAME, app_author=PLUGIN_AUTHOR)
 except Exception:
-    PROJECT_APP_PATH = None
+  PROJECT_APP_PATH = None
 
 __version__ = VERSION
 __author__ = PLUGIN_AUTHOR
 
+from jord.qt_utilities import DockWidgetAreaFlag
 
 MANUAL_REQUIREMENTS = [
     "qgis",
     "osgeo",
     # 'qgis' # not visible to pip?
-]
+    ]
 DEFAULT_PLUGIN_SETTINGS = {
     "RESOURCES_BASE_PATH": f":/{PROJECT_NAME.lower().replace(' ', '_')}",
     "DEFAULT_WIDGET_AREA": DockWidgetAreaFlag.right,
@@ -97,4 +91,4 @@ DEFAULT_PLUGIN_SETTINGS = {
     "SOLVING_PROGRESS_BAR_ENABLED": True,
     "LOGGING_LEVEL": logging.WARNING,
     "REPROJECT_SHAPES": False,
-}
+    }
--- packaging.py
+++ packaging.py
@@ -15,68 +15,64 @@ REQUIREMENTS_FILE = PLUGIN_DIR / "requirements.txt"
 
 logger = logging.getLogger(__name__)
 
-
 def log_subprocess_output(pipe: Pipe) -> None:
-    for line in iter(pipe.readline, b""):  # b'\n'-separated lines
-        logger.info("got line from subprocess: %r", line)
-
+  for line in iter(pipe.readline, b""):  # b'\n'-separated lines
+    logger.info("got line from subprocess: %r", line)
 
 def catching_callable(*args, **kwargs) -> None:
-    try:
-        logger.info(f"{list(args)}, {list(kwargs.items())}")
+  try:
+    logger.info(f"{list(args)}, {list(kwargs.items())}")
 
-        process = Popen(*args, **kwargs, stdout=PIPE, stderr=STDOUT)
-        with process.stdout:
-            log_subprocess_output(process.stdout)
-        exitcode = process.wait()  # 0 means success
-        if exitcode:
-            logger.info("Success")
+    process = Popen(*args, **kwargs, stdout=PIPE, stderr=STDOUT)
+    with process.stdout:
+      log_subprocess_output(process.stdout)
+    exitcode = process.wait()  # 0 means success
+    if exitcode:
+      logger.info("Success")
 
-    except subprocess.CalledProcessError as e:
-        output = (e.stderr, e.stdout, e)
-        logger.warning(output)
-
+  except subprocess.CalledProcessError as e:
+    output = (e.stderr, e.stdout, e)
+    logger.warning(output)
 
 def package_dependencies(
     target_site_packages_dir: Path,
     clean: bool = True,
-) -> None:
-    if target_site_packages_dir.exists():
-        if clean:
-            shutil.rmtree(target_site_packages_dir)
+    ) -> None:
+  if target_site_packages_dir.exists():
+    if clean:
+      shutil.rmtree(target_site_packages_dir)
 
-    target_site_packages_dir.mkdir(parents=True, exist_ok=True)
+  target_site_packages_dir.mkdir(parents=True, exist_ok=True)
 
-    requirements_file_parent_directory = str(
-        REQUIREMENTS_FILE.parent.absolute().as_posix()
-    )
+  submodule_directory = str(
+      REQUIREMENTS_FILE.parent.parent.absolute().as_posix()
+      )
 
-    os.environ["REQUIREMENTS_FILE_PARENT_DIRECTORY"] = (
-        requirements_file_parent_directory
-    )
+  os.environ["SUBMODULE_DIRECTORY"] = (
+      submodule_directory
+  )
 
-    os.environ["ZMQ_PREFIX"] = "bundled"
-    os.environ["ZMQ_BUILD_DRAFT"] = "1"
+  os.environ["ZMQ_PREFIX"] = "bundled"
+  os.environ["ZMQ_BUILD_DRAFT"] = "1"
 
-    catching_callable(
-        [
-            "pip",
-            "install",
-            "-U",
-            "-t",
-            f"{target_site_packages_dir}",
-            "-r",
-            f"{REQUIREMENTS_FILE}",
-            "--break-system-packages",
-            "--verbose",
-            # "--no-binary",
-            # "pyzmq",
-            # "--no-build-isolation",
-        ]
-    )
-
+  catching_callable(
+      [
+          "pip",
+          "install",
+          "-U",
+          "-t",
+          f"{target_site_packages_dir}",
+          "-r",
+          f"{REQUIREMENTS_FILE}",
+          "--break-system-packages",
+          "--verbose",
+          # "--no-binary",
+          # "pyzmq",
+          # "--no-build-isolation",
+          ]
+      )
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    logger.setLevel(logging.INFO)
-    package_dependencies(TARGET_DIR)
+  logging.basicConfig(level=logging.INFO)
+  logger.setLevel(logging.INFO)
+  package_dependencies(TARGET_DIR)
