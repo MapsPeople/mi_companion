b12de7dafa9903f557a9154e8c4b8bd9675482ee
Christian Heider Nielsen <mrnaah@gmail.com>
2024-11-07 08:24:23 +0100

    migration button

---
+++ mi_companion/entry_points/transfer_venue/main.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+import logging
+
+from integration_system.config import MapsIndoors, Settings, set_settings
+from integration_system.migration import migrate_venue
+
+logger = logging.getLogger(__name__)
+
+
+def run(
+    *,
+    from_solution_id: str,
+    to_solution_id: str,
+    venue_admin_id: str,
+    remove_venue_in_from_solution: bool = False,
+) -> None:
+    """
+
+    You can find the venue_admin_id in the venue_polygon layer or in the cms
+
+    :param from_solution_id:
+    :param to_solution_id:
+    :param venue_admin_id:
+    :param remove_venue_in_from_solution: DISABLED FOR NOW!
+    :return:
+    """
+
+    from mi_companion import DEFAULT_PLUGIN_SETTINGS, PROJECT_NAME
+    from jord.qgis_utilities import read_plugin_setting
+
+    sync_module_settings = Settings(
+        mapsindoors=MapsIndoors(
+            username=read_plugin_setting(
+                "MAPS_INDOORS_USERNAME",
+                default_value=DEFAULT_PLUGIN_SETTINGS["MAPS_INDOORS_USERNAME"],
+                project_name=PROJECT_NAME,
+            ),
+            password=read_plugin_setting(
+                "MAPS_INDOORS_PASSWORD",
+                default_value=DEFAULT_PLUGIN_SETTINGS["MAPS_INDOORS_PASSWORD"],
+                project_name=PROJECT_NAME,
+            ),
+            token_endpoint=read_plugin_setting(
+                "MAPS_INDOORS_TOKEN_ENDPOINT",
+                default_value=DEFAULT_PLUGIN_SETTINGS["MAPS_INDOORS_TOKEN_ENDPOINT"],
+                project_name=PROJECT_NAME,
+            ),
+            manager_api_host=read_plugin_setting(
+                "MAPS_INDOORS_MANAGER_API_HOST",
+                default_value=DEFAULT_PLUGIN_SETTINGS["MAPS_INDOORS_MANAGER_API_HOST"],
+                project_name=PROJECT_NAME,
+            ),
+            media_api_host=read_plugin_setting(
+                "MAPS_INDOORS_MEDIA_API_HOST",
+                default_value=DEFAULT_PLUGIN_SETTINGS["MAPS_INDOORS_MEDIA_API_HOST"],
+                project_name=PROJECT_NAME,
+            ),
+        )
+    )
+
+    set_settings(sync_module_settings)
+
+    migrate_venue(
+        from_solution_id=from_solution_id,
+        to_solution_id=to_solution_id,
+        venue_admin_id=venue_admin_id,
+        remove_venue_in_from_solution=remove_venue_in_from_solution,
+    )
