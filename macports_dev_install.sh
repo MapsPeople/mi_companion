89a3e060ea0083788e900138780672be86083db6
Christian Lindbjerg <mrnaah@gmail.com>
2025-08-06 23:59:25 +0200

    Installs development dependencies
    Configures the environment with the necessary packages
    for local development using MacPorts Python 3.13.
    
    This ensures the project's requirements are met.

--- macports_dev_install.sh
+++ macports_dev_install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-#/Applications/MacPorts/"Python 3.13"/IDLE.app/Contents/MacOS/Python -m ensurepip
+/Applications/MacPorts/"Python 3.13"/IDLE.app/Contents/MacOS/Python -m ensurepip
 
-#/Applications/MacPorts/"Python 3.13"/IDLE.app/Contents/MacOS/Python -m pip install --upgrade pip
+/Applications/MacPorts/"Python 3.13"/IDLE.app/Contents/MacOS/Python -m pip install --upgrade pip
 
-#/Applications/MacPorts/"Python 3.13"/IDLE.app/Contents/MacOS/Python -m pip install --upgrade pip setuptools wheel
+/Applications/MacPorts/"Python 3.13"/IDLE.app/Contents/MacOS/Python -m pip install --upgrade pip setuptools wheel
+
+#--no-warn-script-location # to avoid warning about scripts not being on PATH
 
 
+# Install the required packages for the mi_companion project using MacPorts Python 3.13
 export SUBMODULE_DIRECTORY=/Users/maha/Projects/mi_companion
 
 /Applications/MacPorts/"Python 3.13"/IDLE.app/Contents/MacOS/Python -m pip install -r /Users/maha/Projects/mi_companion/mi_companion/requirements.txt
