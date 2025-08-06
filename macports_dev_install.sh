f3ed93172a9890d32f7948d9c176e1bc8f1c4635
Christian Lindbjerg <mrnaah@gmail.com>
2025-08-06 09:36:49 +0200

    MacPorts compatible as well now

---
+++ macports_dev_install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#/Applications/MacPorts/"Python 3.13"/IDLE.app/Contents/MacOS/Python -m ensurepip
+
+#/Applications/MacPorts/"Python 3.13"/IDLE.app/Contents/MacOS/Python -m pip install --upgrade pip
+
+#/Applications/MacPorts/"Python 3.13"/IDLE.app/Contents/MacOS/Python -m pip install --upgrade pip setuptools wheel
+
+
+export SUBMODULE_DIRECTORY=/Users/maha/Projects/mi_companion
+
+/Applications/MacPorts/"Python 3.13"/IDLE.app/Contents/MacOS/Python -m pip install -r /Users/maha/Projects/mi_companion/mi_companion/requirements.txt
